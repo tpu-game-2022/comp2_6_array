d4a15cc8cb5d0720141b250c5420b057f51bcbdf
Shun Furusawa <furu.shun1113@gmail.com>
2022-11-07 16:19:12 +0900

    テストを通しました

--- src/StaticLib/StaticLib.c
+++ src/StaticLib/StaticLib.c
@@ -31,9 +31,37 @@ void release(safe_array* ar)
 	ar->addr = NULL;// 多重解放の防止
 }
 
-// safe_array の要素数を変更する(以前の値は要素が同じ範囲で残す)
-void resize(safe_array* ar, int n)
+void Copy_Array(safe_array* original, safe_array* copy_ar, int n)
 {
+	for (int i = 0; i < n; i++)
+		copy_ar->addr[i] = original->addr[i];
+}
+								
+void resize(safe_array* ar, int n)
+{ 
+	safe_array copy_ar;
+
+	initialize(&copy_ar , n);
+
+	int conti;
+
+	if (n > ar->num)
+		conti = ar->num;
+	else
+		conti = copy_ar.num;
+
+	Copy_Array(&*ar , &copy_ar, conti);
+
+	release(&*ar);
+
+	initialize(&*ar , n);
+
+	Copy_Array(&copy_ar , &*ar , n);
+
+	release(&copy_ar);
+
+
+	
 	// ToOo:配列の要素数を変更しよう！(reallocは禁止)
 }
 
@@ -41,21 +69,34 @@ void resize(safe_array* ar, int n)
 // index が確保されていない場所を指していたら返り値をfalseにする（設定出来たらtrue）
 bool set(const safe_array* ar, int index, int val)
 {
+	if (index < 0 || index > ar->num - 1)
+		return false;
+
+	else
+	{
+		ar->addr[index] = val;
+		return true;
+	}
 	// ToOo:配列の要素を変更しよう！
-	return false;
+	
+	
 }
 
 // safe_array のindex番目の要素を取得する
 // index が確保されていない場所を指していたら0を返す
 int get(const safe_array* ar, int index)
 {
+	if (index < 0 || index > ar->num - 1)
+		return 0;
+	else
+		return ar->addr[index];
 	// ToOo:要素を所得して、indexがおかしかったら0を返そう
-	return -1;
+
 }
 
 // int_array の要素数を取得する
 int size(const safe_array* ar)
 {
 	// ToOo: 配列の要素数を返そう
-	return -1;
+	return ar->num;
 }
